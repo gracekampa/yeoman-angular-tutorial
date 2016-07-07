ce79db6a8aea1cad1047c942f5c433ae5ff7916c
gracekampa <kampa.g@husky.neu.edu>
2016-07-07 15:18:23 -0500

    added a contact page as a test of 'yo angular:view'

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanAngularTutorialApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the yeomanAngularTutorialApp
+ */
+angular.module('yeomanAngularTutorialApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
