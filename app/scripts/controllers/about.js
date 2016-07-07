521cbeb409640738c8114351d363e66f9af7ca17
Grace Kampa <grace.kampa@bnl-consulting.com>
2016-07-07 14:36:36 -0500

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanAngularTutorialApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yeomanAngularTutorialApp
+ */
+angular.module('yeomanAngularTutorialApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanAngularTutorialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeomanAngularTutorialApp
+ */
+angular.module('yeomanAngularTutorialApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
