288a98cb98fd2742caca01857d3401efd4686bf3
gracekampa <kampa.g@husky.neu.edu>
2016-07-12 09:46:37 -0500

    finished designs, added media tags, changed create book to be a popup modal

--- app/scripts/controllers/library.js
+++ app/scripts/controllers/library.js
@@ -9,71 +9,78 @@
  */
 angular.module('yeomanAngularTutorialApp')
   .controller('LibraryCtrl', function ($scope) {
-    $scope.hello = "hello";
 
     $scope.books = [
       {title: 'The Great Gatsby',
        author: 'F. Scott Fitzgerald',
        pages: 180,
-       publishDate: new Date(1925, 3, 10)
+       publishDate: new Date(1925, 3, 10),
+       cover: 'https://i.ytimg.com/vi/ehjTS6AhMJ8/maxresdefault.jpg'
       },
       {title: 'This Side of Paradise',
         author: 'F. Scott Fitzgerald',
         pages: 305,
-        publishDate: new Date(1920, 2, 26)
+        publishDate: new Date(1920, 2, 26),
+        cover: 'https://literarytransgressions.files.wordpress.com/2011/07/fitzparadise.jpg'
       },
       {title: 'Heart of Darkness',
         author: 'Joseph Conrad',
         pages: 72,
-        publishDate: new Date(1899, 1, 23)
+        publishDate: new Date(1899, 1, 23),
+        cover: 'https://msu.edu/~jungahre/transmedia/uploads/5/3/9/9/5399521/4427591.jpg?315'
       },
       {title: 'The Turn of the Screw',
         author: 'Henry James',
         pages: 96,
-        publishDate: new Date(1898, 9, 13)
+        publishDate: new Date(1898, 9, 13),
+        cover: 'http://i.gr-assets.com/images/S/photo.goodreads.com/books/1405982423i/752717._UY200_.jpg'
       },
       {title: 'The Stranger',
         author: 'Albert Camus',
         pages: 123,
-        publishDate: new Date(1942, 2, 13)
+        publishDate: new Date(1942, 2, 13),
+        cover: 'http://bookcritics.org/images/uploads/the-stranger.jpg'
       },
       {title: 'Frankenstein',
         author: 'Mary Shelley',
         pages: 166,
-        publishDate: new Date(1818, 3, 10)
+        publishDate: new Date(1818, 3, 10),
+        cover: 'http://ciervoblanco.club/wp-content/uploads/2014/11/frankenstein-mary-shelley.jpg'
       },
       {title: 'The Metamorphosis',
         author: 'Franz Kafka',
         pages: 55,
-        publishDate: new Date(1915, 3, 16)
+        publishDate: new Date(1915, 3, 16),
+        cover: 'https://qph.ec.quoracdn.net/main-qimg-42df9bdf33d996560684acee63af2845?convert_to_webp=true'
       },
       {title: 'Silas Marner',
         author: 'George Eliot',
         pages: 160,
-        publishDate: new Date(1861, 7, 29)
+        publishDate: new Date(1861, 7, 29),
+        cover: 'http://www.ac-grenoble.fr/cite.scolaire.internationale/Peda/Discipli/CDI/Travaux/grimoires/IMG/jpg_Silas_Marner.jpg'
       },
       {title: 'The Sorrows of Young Werther',
         author: 'Johann Wolfgang von Goethe',
         pages: 96,
-        publishDate: new Date(1774, 8, 29)
+        publishDate: new Date(1774, 8, 29),
+        cover: 'http://66.media.tumblr.com/c66d37bbe23b487f99ac69ba329eca04/tumblr_inline_nbtmst4vLq1sgjss3.jpg'
       },
       {title: 'Passing',
         author: 'Nella Larsen',
         pages: 102,
-        publishDate: new Date(1929, 3, 21)
+        publishDate: new Date(1929, 3, 21),
+        cover: 'http://i1.wp.com/www.audiobooktreasury.com/wp-content/uploads/2015/05/Passing-by-Nella-Larsen-Audiobook.jpg?resize=300%2C449'
       },
       {title: 'The Awakening',
         author: 'Kate Chopin',
         pages: 128,
-        publishDate: new Date(1899, 11, 1)
+        publishDate: new Date(1899, 11, 1),
+        cover: 'http://ahhsapenglish.wikispaces.com/file/view/awakening2.jpg/295630602/awakening2.jpg'
       }
 
     ];
 
     $scope.authorsList = [];
-    $scope.isCreating = false;
-    $scope.startCreating = startCreating;
-    $scope.cancelCreating = cancelCreating;
     $scope.createBook = createBook;
     $scope.resetCreateForm = resetCreateForm;
     $scope.deleteBook = deleteBook;
@@ -87,12 +94,6 @@ angular.module('yeomanAngularTutorialApp')
     function setAuthorsList() {
       $scope.authorsList =_.sortBy($scope.books, 'author');
       $scope.authorsList =_.groupBy($scope.authorsList, 'author');
-      console.log("Authors List", $scope.authorsList);
-      //_($scope.authorsList).map((v, k) => [k, v]).sortBy(0).fromPairs().value()
-      console.log('test set authors');
-
-      // $scope.authorsList = _.keys($scope.authorsList).sort();
-
 
     }
 
@@ -101,45 +102,20 @@ angular.module('yeomanAngularTutorialApp')
       console.log('test watch');
     }, true);
 
-
-    // for each (book in $scope.books) {
-    //   for each (author in book.authors) {
-    //     $scope.authorsList.push(author);
-    //   }
-    // }
-
-    // function getAuthors(){
-    //     _($scope.books).forEach($scope.books, function(value) {
-    //       _(value).forEach(value.authors, function(value) {
-    //         $scope.authorsList.push(value);
-    //       });
-    //     });
-    // }
-    // getAuthors();
-
-    //$scope.authorsList = _.orderBy(authorsList, )
-
-
-    function startCreating() {
-      $scope.isCreating = true;
-    }
-
-    function cancelCreating() {
-      $scope.isCreating = false;
-    }
-
     function resetCreateForm() {
       $scope.newBook = {
         title: '',
         author: '',
         pages: null,
-        publishDate: null
+        publishDate: null,
+        cover: ''
       }
     }
 
     function createBook(newBook) {
       newBook.id = $scope.books.length;
       $scope.books.push(newBook);
+      $('#createBook').modal('hide');
       resetCreateForm();
     }
 
